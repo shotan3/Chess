e459b150572e5a052e68aa4e147c06ad0f3ca956
Shota <shotaandg33@gmail.com>
2022-05-18 01:28:01 +0400

    Added boilerplate code for future development

--- src/com/github/shotan3/chess/utils/ChessMoveValidator.java
+++ src/com/github/shotan3/chess/utils/ChessMoveValidator.java
@@ -1,7 +1,54 @@
 package com.github.shotan3.chess.utils;
 
 public class ChessMoveValidator {
-    public static boolean isValidChessMove(String from, String to, String chessPiece, boolean secondSquareIsEmpty){
-        return true;
+    public static boolean isValidChessMove(String from, String to, String chessPiece, String chessPieceOnSecondSquare) {
+        switch (chessPiece.charAt(0)) {
+            case 'R':
+                return rookCheck(from, to, chessPieceOnSecondSquare);
+            case 'K':
+                if(chessPiece.charAt(1) == 'n'){
+                    return knightCheck(from, to, chessPieceOnSecondSquare);
+                }else{
+                    return kingCheck(from, to, chessPieceOnSecondSquare);
+                }
+            case 'B':
+                return bishopColor(from, to,chessPieceOnSecondSquare);
+            case 'Q':
+                return queenCheck(from, to, chessPieceOnSecondSquare);
+            default:
+                return pawnCheck(from,to,chessPiece, chessPieceOnSecondSquare);
+        }
+    }
+
+    private static boolean rookCheck(String from, String to, String chessPieceOnSecondSquare){
+        return false;
+    }
+
+    private static boolean knightCheck(String from, String to, String chessPieceOnSecondSquare){
+        return false;
+    }
+
+    private static boolean bishopColor(String from, String to, String chessPieceOnSecondSquare){
+        return false;
+    }
+
+    private static boolean queenCheck(String from, String to, String chessPieceOnSecondSquare){
+        return false;
+    }
+
+    private static boolean kingCheck(String from, String to, String chessPieceOnSecondSquare){
+        return false;
+    }
+
+    private static boolean pawnCheck(String from, String to, String pawn , String chessPieceOnSecondSquare) {
+        return false;
+    }
+
+    private static boolean isWhiteSquare(String square) {
+        return ((square.charAt(0) - 'a') + ('8' - square.charAt(1))) % 2 == 0;
+    }
+
+    public static boolean pawnCanPromote(String from, boolean pawnIsEmpty, boolean secondSquareIsEmpty){
+        return false;
     }
 }
