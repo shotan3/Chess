c8e0e6b5cd9021760ecdcc7af1d704cc516fa9bc
Shota <shotaandg33@gmail.com>
2022-05-18 00:07:46 +0400

    Added stub methods for main package

---
+++ src/com/github/shotan3/chess/main/ChessGame.java
@@ -0,0 +1,22 @@
+package com.github.shotan3.chess.main;
+
+public class ChessGame implements Playable{
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void restart() {
+
+    }
+}
---
+++ src/com/github/shotan3/chess/main/Playable.java
@@ -0,0 +1,7 @@
+package com.github.shotan3.chess.main;
+
+public interface Playable {
+    void start();
+    void stop();
+    void restart();
+}
